fca0e262fe445562f521ef814c6722ffc25ff034
Dennis Sergeev <dennis.sergeev@gmail.com>
2018-02-12 14:00:52 +0000

    clean up the variables declaration in vor_partition

--- pmctrack/src/vor_partition.f90
+++ pmctrack/src/vor_partition.f90
@@ -2,66 +2,55 @@ subroutine vor_partition(vor_in, nx, ny, &
      & mlat_out, mlon_out, vor_max_out, mtype_out, &
      & n_max_out, lat, lon, vor_part_max, s_part_out)
 
+use types, only: wp
 use constants, only: pi, ra, rkilo, fillval, nmax, pmax, mx, my
 use params, only : proj, zeta_max0, zeta_min0, int_zeta_min0, gamma
 
-implicit none 
+implicit none
 
-integer(4), intent (in)  :: nx, ny
-real   (4), intent (in)  :: lon(0:nx), lat(0:ny)
-real   (4), intent (in)  :: vor_in(0:nx, 0:ny)
-real   (4), intent (out) :: mlat_out(nmax), mlon_out(nmax), vor_max_out(nmax)
-real   (4), intent (out) :: s_part_out(nmax)
-integer,    intent (out) :: n_max_out
-integer,    intent (out) :: vor_part_max(0:nx, 0:ny)
-integer,    intent (out) :: mtype_out(nmax)
+integer    , intent (in)  :: nx
+integer    , intent (in)  :: ny
+real   (wp), intent (in)  :: lon         (0:nx           )
+real   (wp), intent (in)  :: lat         (      0:ny     )
+real   (wp), intent (in)  :: vor_in      (0:nx, 0:ny     )
+real   (wp), intent (out) :: mlat_out    (           nmax)
+real   (wp), intent (out) :: mlon_out    (           nmax)
+real   (wp), intent (out) :: vor_max_out (           nmax)
+real   (wp), intent (out) :: s_part_out  (           nmax)
+integer    , intent (out) :: n_max_out
+integer    , intent (out) :: vor_part_max(0:nx, 0:ny     )
+integer    , intent (out) :: mtype_out   (           nmax)
+! Local variables
+logical                   :: mij_flag
+logical                   :: remove_flag(nmax)
+integer                   :: remove_num
+integer                   :: n_max
+integer                   :: i, j, ii, jj, m
+integer                   :: i_max, i_max2
+integer                   :: mi, mj, mij(2), mi_tmp, mj_tmp
+integer                   :: i_vor_min, n_vor_min
+integer                   :: n_part
+integer                   :: vor_part    (0:nx, 0:ny      )
+integer                   :: vor_part_tmp(0:nx, 0:ny      )
+integer                   :: buf_mij     (2,          nmax)
+integer                   :: buf_mij_out (2,          nmax)
+integer                   :: part_num    (            nmax)
+integer                   :: mtype_part  (            nmax)
+integer                   :: mtype       (            nmax, 2)
+integer                   :: surr8_buf   (2,        1:pmax)
+integer                   :: p
+real   (wp)               :: mlat        (            nmax)
+real   (wp)               :: mlon        (            nmax)
+real   (wp)               :: vor_max     (            nmax)
+real   (wp)               :: lonin, latin
+real   (wp)               :: vor         (0:nx, 0:ny      )
+real   (wp)               :: max_tmp
+real   (wp)               :: vor_min_tmp
+real   (wp)               :: del_vor_max_tmp
+real   (wp)               :: l(nmax), l_min(nmax), theta_l
 
-! LOCAL VARIABLES
-real   (4) :: vor_out(0:nx, 0:ny)
 
-real   (4) :: mlat(nmax), mlon(nmax), vor_max(nmax)
-integer(4) :: n_max
-real   (4) :: lonin, latin
-
-real   (4) :: vor(0:nx, 0:ny)
-integer(4) :: i, j, ii, jj, m
-integer(4) :: i_max, i_max2
-real   (4) :: max_tmp
-real   (4) :: vor_min_tmp
-integer(4) :: mi, mj, mij(2), mi_tmp, mj_tmp
-integer(4) :: i_vor_min, n_vor_min
-logical(4) :: mij_flag
-! logical(4) :: topo_flag
-integer(4) :: n_part !, i_part
-integer(4) :: vor_part(0:nx, 0:ny), vor_part_tmp(0:nx, 0:ny)
-real   (4) :: vor_part_r(0:nx, 0:ny), vor_part_max_r(0:nx, 0:ny)
-integer(4) :: buf_mij(2, nmax), buf_mij_out(2, nmax)
-
-integer(4) :: part_num(nmax), part_num_out(nmax)
-integer(4) :: mtype_part(nmax), mtype(nmax, 2)
-real   (4) :: partmax(nmax)
-
-real   (4) :: del_vor_max_tmp
-
-! real   (4) :: d, theta_d
-! real   (4) :: d_cf, theta_d_cf
-logical(4) :: remove_flag(nmax)
-integer(4) :: remove_num
-
-real   (4) :: l(nmax), l_min(nmax), theta_l
-
-!----cold front
-! integer(4) :: width, width_max, pnum
-! integer(4) :: j_n, j_s, i_w, i_e, i_n, i_s
-! logical    :: flag_coldfront
-! logical    :: flag_one
-
-!----synoptic
-! real   (4) :: size_vor
-
-integer(4) :: surround8_buf(1:2, 1:pmax)
-integer(4) :: p=0
-
+p = 0
 
 lonin = lon(1) - lon(0)
 latin = lat(1) - lat(0)
@@ -69,36 +58,27 @@ latin = lat(1) - lat(0)
 n_max = 0
 n_part = 0
 max_tmp = 0
+buf_mij = -1
+vor_min_tmp = zeta_max0
+vor_part = 0
+part_num = 0
+vor_part_max = 0
+mtype(:, :) = 0
+mtype_out(:) = 0
+mtype_part(:) = 0
 
-buf_mij=-1
-
-vor_min_tmp=zeta_max0
-
-vor_out(0:nx, 0:ny)=fillval
-
-vor_out(0, ny)=-1
-
-vor_part=0
-part_num=0
-vor_part_max=0
-partmax=0.
-
-mtype(:, :)=0
-
-mtype_out(:)=0
-mtype_part(:)=0
-do j=0,ny
-  do i=0,nx
-    vor(i,j)=vor_in(i,j)
-  end do
-end do
+do j = 0, ny
+  do i = 0, nx
+    vor(i, j) = vor_in(i, j)
+  enddo
+enddo
 
 max_tmp=maxval(vor)
 n_vor_min=int((max_tmp-zeta_max0)/int_zeta_min0)+1
 
 do
   p=0
- 
+
 
   max_tmp=maxval(vor)
   mij=maxloc(vor)
@@ -107,7 +87,6 @@ do
   if(max_tmp<=zeta_max0)exit
   n_part=n_part+1
   vor_part(mi,mj)=n_part
-  partmax(n_part) = vor(mi, mj)
 
   vor(mi,mj)=0.
 
@@ -117,23 +96,23 @@ do
        & mi+mx(m) <= nx .and. &
        & mj+my(m) >= 0  .and. &
        & mj+my(m) <= ny       ) then
-      if(vor(mi+mx(m),mj+my(m))>zeta_min0)then     
+      if(vor(mi+mx(m),mj+my(m))>zeta_min0)then
         vor_part(mi+mx(m),mj+my(m))=n_part
         vor(mi+mx(m),mj+my(m))=0.
 
         if (p < pmax) then
-          p = p + 1 
-          surround8_buf(1,p) = mi+mx(m)
-          surround8_buf(2,p) = mj+my(m)
-        end if
-      end if
+          p = p + 1
+          surr8_buf(1,p) = mi+mx(m)
+          surr8_buf(2,p) = mj+my(m)
+        endif
+      endif
     endif
-  end do
+  enddo
 
-  do 
+  do
     if(p==0)exit
-    mi_tmp=surround8_buf(1,p)
-    mj_tmp=surround8_buf(2,p)
+    mi_tmp=surr8_buf(1,p)
+    mj_tmp=surr8_buf(2,p)
 !#ifdef debug
 !    print*, 'vor_partition(143):', mi_tmp, mj_tmp
 !#endif
@@ -150,24 +129,24 @@ do
 
             vor(mi_tmp+mx(m),mj_tmp+my(m))=0.
             if (p < pmax) then
-              p = p + 1 
-              surround8_buf(1,p) = mi_tmp+mx(m)
-              surround8_buf(2,p) = mj_tmp+my(m)
-            end if
-          end if
-        end if
-      end if
-    end do
-  end do
+              p = p + 1
+              surr8_buf(1,p) = mi_tmp+mx(m)
+              surr8_buf(2,p) = mj_tmp+my(m)
+            endif
+          endif
+        endif
+      endif
+    enddo
+  enddo
 
 
 !--------------- Check cold front and synoptic low----------------
     call cf_synop_check(vor_in(0:nx,0:ny),vor_part(0:nx,0:ny),n_part,nx,ny,proj,&
          &lon(0:nx),lat(0:ny),mtype_part(n_part))
 
-      n_max=n_max+1         
+      n_max=n_max+1
       buf_mij(1,n_max)=mi
-      buf_mij(2,n_max)=mj         
+      buf_mij(2,n_max)=mj
 
 
       mlon(n_max)=lon(mi)
@@ -178,17 +157,16 @@ do
 
 
 
-    end do
+    enddo
 
   vor=0.
   do j=0,ny
     do i=0,nx
       if(vor_part(i,j)>0)then
         vor(i,j)=vor_in(i,j)
-        !           if(vor_out(i,j)==0)vor_out(i,j)=vor_part_max(i,j)
-      end if
-    end do
-  end do
+      endif
+    enddo
+  enddo
 
   vor_part_tmp=0
 
@@ -220,23 +198,23 @@ do
            & mi+mx(m) <= nx .and. &
            & mj+my(m) >= 0  .and. &
            & mj+my(m) <= ny       ) then
-          if(vor(mi+mx(m),mj+my(m))>vor_min_tmp)then     
+          if(vor(mi+mx(m),mj+my(m))>vor_min_tmp)then
             vor_part_tmp(mi+mx(m),mj+my(m))=n_part
             vor(mi+mx(m),mj+my(m))=0.
 
             if (p < pmax) then
-              p = p + 1 
-              surround8_buf(1,p) = mi+mx(m)
-              surround8_buf(2,p) = mj+my(m)
-            end if
-          end if
-        end if
-      end do
+              p = p + 1
+              surr8_buf(1,p) = mi+mx(m)
+              surr8_buf(2,p) = mj+my(m)
+            endif
+          endif
+        endif
+      enddo
 
-      do 
+      do
         if(p==0)exit
-        mi_tmp=surround8_buf(1,p)
-        mj_tmp=surround8_buf(2,p)
+        mi_tmp=surr8_buf(1,p)
+        mj_tmp=surr8_buf(2,p)
         p=p-1
 
         do m=1,8
@@ -250,15 +228,15 @@ do
                 vor(mi_tmp+mx(m),mj_tmp+my(m))=0.
 
                 if (p < pmax) then
-                  p = p + 1 
-                  surround8_buf(1,p) = mi_tmp+mx(m)
-                  surround8_buf(2,p) = mj_tmp+my(m)
-                end if
-              end if
-            end if
-          end if
-        end do
-      end do
+                  p = p + 1
+                  surr8_buf(1,p) = mi_tmp+mx(m)
+                  surr8_buf(2,p) = mj_tmp+my(m)
+                endif
+              endif
+            endif
+          endif
+        enddo
+      enddo
 
 
 
@@ -266,26 +244,26 @@ do
       mij_flag=.true.
       do i_max=1,n_max
         if(buf_mij(1,i_max)==mi.and.buf_mij(2,i_max)==mj)mij_flag=.false.
-      end do
+      enddo
 
       if(mij_flag)then
         del_vor_max_tmp=gamma*max_tmp
 
         if(max_tmp-vor_min_tmp>del_vor_max_tmp)then
 
-          n_max=n_max+1         
+          n_max=n_max+1
           buf_mij(1,n_max)=mi
-          buf_mij(2,n_max)=mj         
+          buf_mij(2,n_max)=mj
           mlon(n_max)=lon(mi)
           mlat(n_max)=lat(mj)
           vor_max(n_max)=max_tmp
           part_num(n_max)=vor_part(mi,mj)
           mtype(n_max,1)=mtype_part(part_num(n_max))
 
-        end if
-      end if
+        endif
+      endif
 
-    end do
+    enddo
 
     vor=0.
     do j=0,ny
@@ -293,12 +271,12 @@ do
         if(vor_part_tmp(i,j)>0)then
           vor(i,j)=vor_in(i,j)
 
-        end if
-      end do
-    end do
+        endif
+      enddo
+    enddo
     vor_part_tmp=0
 
-  end do
+  enddo
 
 
 
@@ -306,8 +284,8 @@ do
   do j=0,ny
     do i=0,nx
       vor(i,j)=vor_in(i,j)
-    end do
-  end do
+    enddo
+  enddo
 
 
 !--------------------------------------------------------
@@ -316,49 +294,29 @@ do
 
 !   do i_max=1,n_max
 !     if(vor_max(i_max)<0.3e-3)then
-     
+
 !       if(mlon(i_max)<135.and.mlon(i_max)>125.and.&
 !            &mlat(i_max)<44.and.mlat(i_max)>38)then
 !         do j=0,ny
 !           do i=0,nx
 !             if(abs(lon(i)-mlon(i_max))<0.5.and.abs(lat(j)-mlat(i_max))<0.4)then
 !               if(land_flag(i,j))remove_flag(i_max)=.true.
-!             end if
-!           end do
-!         end do
+!             endif
+!           enddo
+!         enddo
 !       elseif(mlon(i_max)>135.and.mlon(i_max)<139.and.&
 !            &mlat(i_max)<47.and.mlat(i_max)>44)then
 !         do j=0,ny
 !           do i=0,nx
 !             if(abs(lon(i)-mlon(i_max))<0.5.and.abs(lat(j)-mlat(i_max))<0.4)then
 !               if(land_flag(i,j))remove_flag(i_max)=.true.
-!             end if
-!           end do
-!         end do
-!       end if
-  
-!     end if
-!   end do
-  
+!             endif
+!           enddo
+!         enddo
+!       endif
 
-!   do i_part=1,maxval(part_num,1)
-!     topo_flag=.true.
-!     do i_max=1,n_max
-!       if(part_num(i_max)==i_part.and..not.remove_flag(i_max))then
-!         topo_flag=.false.
-!       end if
-!     end do
-!     if(topo_flag)then
-!       do j=0,ny
-!         do i=0,nx
-!           if(vor_part(i,j)==i_part)vor_part(i,j)=0
-!         end do
-!       end do
-!     end if
-!   end do
-
-
-
+!     endif
+!   enddo
 
 
   remove_num=0
@@ -369,8 +327,8 @@ do
       do j=0,ny
         do i=0,nx
           if(vor_part(i,j)==i_max)vor_part(i,j)=-10
-        end do
-      end do
+        enddo
+      enddo
     else
       n_max_out=n_max_out+1
       mlon_out(n_max_out)=mlon(i_max)
@@ -379,16 +337,9 @@ do
 !      mtype_out(n_max_out)=mtype(i_max,1)+mtype(i_max,2)
       vor_max_out(n_max_out)=vor_max(i_max)
       buf_mij_out(1:2,n_max_out)=buf_mij(1:2,i_max)
-      part_num_out(n_max_out)=part_num(i_max)
-    end if
-  end do
+    endif
+  enddo
 
-    
-  
-  
-
-      
-!---------------------------------------------------------
 
 !----- Partation ----
 
@@ -404,7 +355,7 @@ do
 
       if(vor_min_tmp<zeta_min0)exit
 
-      
+
 
       p=0
       max_tmp=vor_max_out(i_max)
@@ -418,25 +369,25 @@ do
 
       do m=1,8
         if(mi+mx(m)>=0.and.mi+mx(m)<=nx.and.mj+my(m)>=0.and.mj+my(m)<=ny)then
-          if(vor(mi+mx(m),mj+my(m))>vor_min_tmp)then  
+          if(vor(mi+mx(m),mj+my(m))>vor_min_tmp)then
             vor_part_tmp(mi+mx(m),mj+my(m))=n_part
             vor(mi+mx(m),mj+my(m))=0
 
 
 
             if (p < pmax) then
-              p = p + 1 
-              surround8_buf(1,p) = mi+mx(m)
-              surround8_buf(2,p) = mj+my(m)
-            end if
-          end if
-        end if
-      end do
+              p = p + 1
+              surr8_buf(1,p) = mi+mx(m)
+              surr8_buf(2,p) = mj+my(m)
+            endif
+          endif
+        endif
+      enddo
 
-      do 
+      do
         if(p==0)exit
-        mi_tmp=surround8_buf(1,p)
-        mj_tmp=surround8_buf(2,p)
+        mi_tmp=surr8_buf(1,p)
+        mj_tmp=surr8_buf(2,p)
         p=p-1
 
         do m=1,8
@@ -450,15 +401,15 @@ do
 
 
                 if (p < pmax) then
-                  p = p + 1 
-                  surround8_buf(1,p) = mi_tmp+mx(m)
-                  surround8_buf(2,p) = mj_tmp+my(m)
-                end if
-              end if
-            end if
-          end if
-        end do
-      end do
+                  p = p + 1
+                  surr8_buf(1,p) = mi_tmp+mx(m)
+                  surr8_buf(2,p) = mj_tmp+my(m)
+                endif
+              endif
+            endif
+          endif
+        enddo
+      enddo
 
 
       mij_flag=.true.
@@ -470,29 +421,29 @@ do
             do i_max2=1,n_max_out
               if(buf_mij_out(1,i_max2)==i.and.buf_mij_out(2,i_max2)==j&
                    &.and.i_max2/=i_max)   mij_flag=.false.
-            end do
+            enddo
 
-          end if
-        end do
-      end do
+          endif
+        enddo
+      enddo
 
       if(.not.mij_flag)exit
 
       do j=0,ny
         do i=0,nx
           if(vor_part_tmp(i,j)==i_max.and.vor_part_max(i,j)==0)vor_part_max(i,j)=i_max
-        end do
-      end do
+        enddo
+      enddo
 
       do j=0,ny
         do i=0,nx
             vor(i,j)=vor_in(i,j)
-        end do
-      end do
+        enddo
+      enddo
 
-    end do
+    enddo
 
-  end do
+  enddo
 
 
 !--------------------------------------------------
@@ -522,22 +473,22 @@ do
                 endif
               elseif(proj==2)then
                 l(vor_part_max(ii,jj))=sqrt((lon(i)-lon(ii))**2+(lat(j)-lat(jj))**2)
-              end if
+              endif
               if(l(vor_part_max(ii,jj))<l_min(vor_part_max(ii,jj)))then
                 l_min(vor_part_max(ii,jj))=l(vor_part_max(ii,jj))
-              end if
-            end if
-          end do
-        end do
-        
+              endif
+            endif
+          enddo
+        enddo
+
 
 
         if(minval(l_min)<0.9e9)then
           vor_part_tmp(i,j)=minloc(l_min,1)
-        end if
+        endif
 
 
-      end if
+      endif
 
       if(vor_part(i,j)==-10.and.vor_part_max(i,j)==0)then
 
@@ -555,104 +506,62 @@ do
               endif
             elseif(proj==2)then
               l(vor_part_max(ii,jj))=sqrt((lon(i)-lon(ii))**2+(lat(j)-lat(jj))**2)
-            end if
-              
+            endif
+
             if(l(vor_part_max(ii,jj))<l_min(vor_part_max(ii,jj)))then
               l_min(vor_part_max(ii,jj))=l(vor_part_max(ii,jj))
-            end if
-            
-          end do
-        end do
-        
+            endif
+
+          enddo
+        enddo
+
 
 
         if(minval(l_min)<0.9e9)then
           vor_part_tmp(i,j)=minloc(l_min,1)
-        end if
+        endif
 
 
-      end if
+      endif
 
 
-    end do
-  end do
+    enddo
+  enddo
 
   do j=0,ny
     do i=0,nx
       if(vor_part_tmp(i,j)/=0.and.vor_part_max(i,j)==0)then
         vor_part_max(i,j)=vor_part_tmp(i,j)
 
-      end if
-    end do
-  end do
+      endif
+    enddo
+  enddo
 
 !--------------------------------------------------
 
 
 
-  
+
 
 !--------------------------------------------
 
 !------ calclate size of vortex -----
-  
+
 
   s_part_out=0.
   do j=0,ny
     do i=0,nx
       if(vor_part_max(i,j)>0)then
-        
+
         if(proj==1)then
           s_part_out(vor_part_max(i,j))=s_part_out(vor_part_max(i,j))&
                &+lonin*(pi/180.)*ra/rkilo*cos(lat(j)*pi/180.)*latin*(pi/180.)*ra/rkilo
         elseif(proj==2)then
           s_part_out(vor_part_max(i,j))=s_part_out(vor_part_max(i,j))&
                &+lonin*latin*1.0e-6
-        end if
-      end if
-    end do
-  end do
+        endif
+      endif
+    enddo
+  enddo
 
-
-  do j=0,ny
-    do i=0,nx
-      if(vor_part_max(i,j)>0)then
-        vor_out(i,j)=real(vor_part_max(i,j))
-      end if
-    end do
-  end do
-
-
-  vor_part_r=fillval
-  vor_part_max_r=fillval
-
-  vor_part_r(0,ny)=-1.
-  vor_part_max_r(0,ny)=-1.
-  
-
-  do j=0,ny
-    do i=0,nx
-      if(vor_part_max(i,j)>0)then
-        vor_part_max_r(i,j)=real(vor_part_max(i,j))
-      end if
-    end do
-  end do
-
-  do j=0,ny
-    do i=0,nx
-      if(vor_part(i,j)/=0)then
-        vor_part_r(i,j)=real(vor_part(i,j))
-      end if
-    end do
-  end do
-
-!  write (98)vor_part_r
-!  write (98)vor_part_max_r
-
-
-!  write (*,*)'vor_partition finish!'
-
-
-
-  return
 end subroutine vor_partition
