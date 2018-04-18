15d3e99e3a3d086f275fd3af532762fc0b8dfb97
Dennis Sergeev <dennis.sergeev@gmail.com>
2018-04-18 17:31:55 +0000

    finish writing launcher

--- launcher/launch_track.py
+++ launcher/launch_track.py
@@ -17,40 +17,61 @@ OUTDIR = os.path.join(os.getenv('PROJ'), 'pmctrack', 'output', '{dataset}',
                       '{run}', '{winter}')
 STORE_DIR = 'configs'
 datasets = ['era5', 'interim']
-DEF_CONF_FNAME = os.path.join(STORE_DIR, 'settings.conf')
+walltimes = ['3:0:0', '0:15:0']
+DEF_CONF_FNAME = os.path.join(STORE_DIR, 'dummy_settings.conf')
 CONF_MASK = os.path.join(STORE_DIR, '{dataset}_{run}_settings.conf')
+
+LOG_DIR = os.path.join(os.getenv('PROJ'), 'pmctrack', 'logs')
+EXE = os.path.join(os.getenv('HOME'), 'pmctrack', 'track.x')
+# cd $PBS_O_WORKDIR  
+CMD_MASK = ('qsub'
+            ' -N pmctrack_{dataset}_{run}'
+            ' -q shared'
+            ' -l select=1'
+            ' -l walltime={walltime}'
+            ' -o {log_dir}'
+            ' -e {log_dir}'
+            ' -- {exe} {conf}')
 
 
 with open('runs_grid.json', 'r') as jf:
     runs_dict = json.load(jf)
 
 
-for dataset in datasets:
+for dataset, walltime in zip(datasets, walltimes):
     with open(DEF_CONF_FNAME, 'r') as fp:
         def_conf = fp.readlines()
 
-    aux_dict['datadir'] = DATADIR.format(dataset=dataset)
-    aux_dict['prefix_lvl'] = '{dataset}.an.pl.'.format(dataset=dataset)
-    aux_dict['prefix_sfc'] = '{dataset}.an.sfc.'.format(dataset=dataset)
+    aux_dict['datadir'] = '"{}"'.format(DATADIR.format(dataset=dataset))
+    aux_dict['prefix_lvl'] = '"{dataset}.an.pl."'.format(dataset=dataset)
+    aux_dict['prefix_sfc'] = '"{dataset}.an.sfc."'.format(dataset=dataset)
 
     for run, params_dict in runs_dict.items():
-        aux_dict['outdir'] = OUTDIR.format(dataset=dataset, run=run,
-                                           winter=winter)
+        aux_dict['outdir'] = '"{}"'.format(OUTDIR.format(dataset=dataset,
+                                                         run=run,
+                                                         winter=winter))
         target_conf = []
         for line in def_conf:
             new_l = line.rstrip('\n')
-            if not line.startswith('#'):
-                old_param, = line.split('=')
-                d = dict(**params_dict, **aux_dict)
+            if not new_l.startswith('#'):
+                old_param = new_l.split('=')[0]
+                d = params_dict.copy()
+                d.update(aux_dict)
                 for param, new_val in d.items():
                     if old_param == param:
                         new_l = '{}={}'.format(param, new_val)
             target_conf.append(new_l)
 
-        conf_file = CONF_MASK.format(dataset=dataset, run=run)
+        conf_file = os.path.abspath(CONF_MASK.format(dataset=dataset,
+                                                     run=run))
         with open(conf_file, 'w') as fp:
             for line in target_conf:
                 fp.write(line+'\n')
 
-        subp_args = ([])
+        subp_args = CMD_MASK.format(dataset=dataset, run=run,
+                                    walltime=walltime,
+                                    log_dir=LOG_DIR, exe=EXE,
+                                    conf=conf_file).split()
+        # ['cp {} {}'.format(conf_file, aux_dict['outdir'])])
         print(subp_args)
+        sb.call(subp_args)
