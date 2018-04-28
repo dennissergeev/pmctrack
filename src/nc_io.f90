8d85b3d373c6c51a96991695347121329a22e018
Dennis Sergeev <dennis.sergeev@gmail.com>
2018-04-28 16:21:48 +0100

    add more descriptive messages to nc_io module

--- src/nc_io.f90
+++ src/nc_io.f90
@@ -5,6 +5,7 @@ module nc_io
   use types, only : wp
 
   implicit none
+  character(len=256) :: msg
 
 contains
   subroutine get_dims(nc_file_name, dim_names, ntime, nlvls, nlats, nlons)
@@ -21,18 +22,20 @@ contains
     integer                       , dimension(4)  :: dims
     integer                                       :: i
 
-    call check( nf90_open(nc_file_name, nf90_nowrite, ncid) )
+    write(msg, '(A)') "get_dims( "//trim(nc_file_name)//", ... )"
+
+    call check( nf90_open(nc_file_name, nf90_nowrite, ncid), msg )
 
     do i = 1, size(dim_names)
-      call check( nf90_inq_dimid(ncid, dim_names(i), dimid) )
-      call check( nf90_inquire_dimension(ncid, dimid, len=dims(i)) )
+      call check( nf90_inq_dimid(ncid, dim_names(i), dimid), msg )
+      call check( nf90_inquire_dimension(ncid, dimid, len=dims(i)), msg )
     end do
     ntime = dims(1)
     nlvls = dims(2)
     nlats = dims(3)
     nlons = dims(4)
 
-    call check( nf90_close(ncid) )
+    call check( nf90_close(ncid), msg )
   end subroutine get_dims
 
 
@@ -51,12 +54,14 @@ contains
     character(len=*), parameter                   :: cal_fmt = "since %Y-%m-%d %H:%M:%S"
     integer                                       :: i
 
-    call check( nf90_open(nc_file_name, nf90_nowrite, ncid) )
+    write(msg, '(A)') "get_time( "//trim(nc_file_name)//", ... )"
 
-    call check( nf90_inq_varid(ncid, time_name, var_id) )
-    call check( nf90_get_var(ncid, var_id, time) )
+    call check( nf90_open(nc_file_name, nf90_nowrite, ncid), msg )
 
-    call check( nf90_get_att(ncid, var_id, "units", units) )
+    call check( nf90_inq_varid(ncid, time_name, var_id), msg )
+    call check( nf90_get_var(ncid, var_id, time), msg )
+
+    call check( nf90_get_att(ncid, var_id, "units", units), msg )
 
     ! "hours since 1900-01-01 00:00:0.0"
     units_date = trim(units)
@@ -86,7 +91,7 @@ contains
       stop
     endif
 
-    call check( nf90_close(ncid) )
+    call check( nf90_close(ncid), msg )
   end subroutine get_time
 
 
@@ -105,20 +110,22 @@ contains
     integer                                       :: ncid
     integer                                       :: var_id
 
-    call check( nf90_open(nc_file_name, nf90_nowrite, ncid) )
+    write(msg, '(A)') "get_coords( "//trim(nc_file_name)//", ... )"
 
-    call check( nf90_inq_varid(ncid, dim_names(1), var_id) )
+    call check( nf90_open(nc_file_name, nf90_nowrite, ncid), msg )
+
+    call check( nf90_inq_varid(ncid, dim_names(1), var_id), msg )
     call check( nf90_get_var(ncid, var_id, time,  &
                              start=(/time_idx/), &
-                             count=(/      nt/)) )
-    call check( nf90_inq_varid(ncid, dim_names(2), var_id) )
-    call check( nf90_get_var(ncid, var_id, lvls) )
-    call check( nf90_inq_varid(ncid, dim_names(3), var_id) )
-    call check( nf90_get_var(ncid, var_id, lats) )
-    call check( nf90_inq_varid(ncid, dim_names(4), var_id) )
-    call check( nf90_get_var(ncid, var_id, lons) )
+                             count=(/      nt/)), msg )
+    call check( nf90_inq_varid(ncid, dim_names(2), var_id), msg )
+    call check( nf90_get_var(ncid, var_id, lvls), msg )
+    call check( nf90_inq_varid(ncid, dim_names(3), var_id), msg )
+    call check( nf90_get_var(ncid, var_id, lats), msg )
+    call check( nf90_inq_varid(ncid, dim_names(4), var_id), msg )
+    call check( nf90_get_var(ncid, var_id, lons), msg )
 
-    call check( nf90_close(ncid) )
+    call check( nf90_close(ncid), msg )
   end subroutine get_coords
 
 
@@ -137,20 +144,23 @@ contains
     real(wp)                                      :: scale_factor, add_offset
     integer                                       :: shp(3)
 
+    write(msg, '(A)') "get_xyz_from_xyzt( "//trim(nc_file_name)//", ... )"
+
     shp = shape(var_data)
 
-    call check( nf90_open(nc_file_name, nf90_nowrite, ncid) )
-    call check( nf90_inq_varid(ncid, var_name, var_id) )
+    call check( nf90_open(nc_file_name, nf90_nowrite, ncid), msg )
+    call check( nf90_inq_varid(ncid, var_name, var_id), msg )
     call check( nf90_get_var(ncid, var_id, var_data,  &
                              start=(/     1,      1, lvl_idx, time_idx/), &
-                             count=(/shp(1), shp(2),      nz,        1/)) )
+                             count=(/shp(1), shp(2),      nz,        1/)), &
+                msg )
 
-    call check( nf90_get_att(ncid, var_id, "scale_factor", scale_factor) )
-    call check( nf90_get_att(ncid, var_id, "add_offset", add_offset) )
+    call check( nf90_get_att(ncid, var_id, "scale_factor", scale_factor), msg )
+    call check( nf90_get_att(ncid, var_id, "add_offset", add_offset), msg )
   
     var_data = scale_factor * var_data + add_offset
 
-    call check( nf90_close(ncid) )
+    call check( nf90_close(ncid), msg )
   end subroutine get_xyz_from_xyzt
 
 
@@ -168,20 +178,22 @@ contains
     real(wp)                                      :: scale_factor, add_offset
     integer                                       :: shp(2)
 
+    write(msg, '(A)') "get_xy_from_xyzt( "//trim(nc_file_name)//", ... )"
+
     shp = shape(var_data)
 
-    call check( nf90_open(nc_file_name, nf90_nowrite, ncid) )
+    call check( nf90_open(nc_file_name, nf90_nowrite, ncid), msg )
 
-    call check( nf90_inq_varid(ncid, var_name, var_id) )
+    call check( nf90_inq_varid(ncid, var_name, var_id), msg )
     call check( nf90_get_var(ncid, var_id, var_data,  &
                              start=(/     1,      1, lvl_idx, time_idx/), &
-                             count=(/shp(1), shp(2),       1,       1/)) )
-    call check( nf90_get_att(ncid, var_id, "scale_factor", scale_factor) )
-    call check( nf90_get_att(ncid, var_id, "add_offset", add_offset) )
+                             count=(/shp(1), shp(2),       1,       1/)), msg )
+    call check( nf90_get_att(ncid, var_id, "scale_factor", scale_factor), msg )
+    call check( nf90_get_att(ncid, var_id, "add_offset", add_offset), msg )
   
     var_data = scale_factor * var_data + add_offset
 
-    call check( nf90_close(ncid) )
+    call check( nf90_close(ncid), msg )
   end subroutine get_xy_from_xyzt
 
 
@@ -197,20 +209,22 @@ contains
     real(wp)                                      :: scale_factor, add_offset
     integer                                       :: shp(2)
 
+    write(msg, '(A)') "get_xy_from_xyt( "//trim(nc_file_name)//", ... )"
+
     shp = shape(var_data)
 
-    call check( nf90_open(nc_file_name, nf90_nowrite, ncid) )
+    call check( nf90_open(nc_file_name, nf90_nowrite, ncid), msg )
 
     call check( nf90_inq_varid(ncid, var_name, var_id) )
     call check( nf90_get_var(ncid, var_id, var_data,  &
                              start=(/     1,      1, time_idx/), &
-                             count=(/shp(1), shp(2),       1/)) )
-    call check( nf90_get_att(ncid, var_id, "scale_factor", scale_factor) )
-    call check( nf90_get_att(ncid, var_id, "add_offset", add_offset) )
+                             count=(/shp(1), shp(2),       1/)), msg )
+    call check( nf90_get_att(ncid, var_id, "scale_factor", scale_factor), msg )
+    call check( nf90_get_att(ncid, var_id, "add_offset", add_offset), msg )
   
     var_data = scale_factor * var_data + add_offset
 
-    call check( nf90_close(ncid) )
+    call check( nf90_close(ncid), msg )
   end subroutine get_xy_from_xyt
 
 
@@ -224,26 +238,35 @@ contains
     integer                                       :: var_id
     real(wp)                                      :: scale_factor, add_offset
 
-    call check( nf90_open(nc_file_name, nf90_nowrite, ncid) )
+    write(msg, '(A)') "get_data_2d( "//trim(nc_file_name)//", ... )"
 
-    call check( nf90_inq_varid(ncid, var_name, var_id) )
-    call check( nf90_get_var(ncid, var_id, var_data) )
-    call check( nf90_get_att(ncid, var_id, "scale_factor", scale_factor) )
-    call check( nf90_get_att(ncid, var_id, "add_offset", add_offset) )
-  
+    call check( nf90_open(nc_file_name, nf90_nowrite, ncid), msg )
+
+    call check( nf90_inq_varid(ncid, var_name, var_id), msg )
+    call check( nf90_get_var(ncid, var_id, var_data), msg )
+    call check( nf90_get_att(ncid, var_id, "scale_factor", scale_factor), msg )
+    call check( nf90_get_att(ncid, var_id, "add_offset", add_offset), msg )
+    
     var_data = scale_factor * var_data + add_offset
 
-    call check( nf90_close(ncid) )
+    call check( nf90_close(ncid), msg )
   end subroutine get_data_2d
 
 
-  subroutine check(status)
-    integer, intent (in) :: status
+  subroutine check(stat, trace_msg)
+    integer                   , intent(in) :: stat
+    character(len=*), optional, intent(in) :: trace_msg
 
-    if (status /= nf90_noerr) then 
-      write(*, *) "status:", status
-      write(*, *) trim(nf90_strerror(status))
-      stop "Stopped"
+    if (stat /= nf90_noerr) then
+      if (present(trace_msg)) then
+        write(*, *) "Traceback: nc_io.f90 module"
+        write(*, *) "FatalError occured when calling "
+        write(*, *) "(nc_io.f90) "//trim(trace_msg)
+        write(*, *)
+      endif
+      write(*, '(A,A,I5,A)') "    "//trim(nf90_strerror(stat)), " (code ", stat, ")"
+      write(*, *) "Terminating the program"
+      stop
     end if
   end subroutine check  
 
